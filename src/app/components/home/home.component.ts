e54ef8138b86f52de22535da70c9fa2b7cd7687b
saiful Islam <saiful.islam5@northsouth.edu>
2022-08-03 15:52:45 +0600

    #addToCart

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -76,5 +76,47 @@ export class HomeComponent implements OnInit {
    }
 
   }
+  itemsCart:any=[];
+  addCart(category:any){
+    //console.log(category);
+    let cartDataNull = localStorage.getItem('localCart');
+    if(cartDataNull == null){
+      let storedataGet : any =[];
+      storedataGet.push(category);
+      localStorage.setItem('localCart',JSON.stringify(storedataGet));
+    }
+    else{
+      var id =category.prodId;
+      let index:number=-1;
+      this.itemsCart= JSON.parse(localStorage.getItem('localCart')!);
+      for(let i= 0; i<this.itemsCart.length;i++){
+        if(parseInt(id) ===parseInt(this.itemsCart[i].prodId)){
+          this.itemsCart[i].qnt = category.qnt;
+          index = i;
+          break;
+
+        }
+
+      }
+      if(index == -1) {
+        this.itemsCart.push(category);
+        localStorage.setItem('localCart',JSON.stringify(this.itemsCart));
+
+      }
+      else{
+        localStorage.setItem('localCart',JSON.stringify(this.itemsCart));
+      }
+
+
+
+
+    }
+
+
+
+
+
+
+  }
 
 }
