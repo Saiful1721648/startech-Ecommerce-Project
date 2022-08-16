cc12ec752b3af65dfa90a9d6497dd9faad566279
saiful Islam <saiful.islam5@northsouth.edu>
2022-08-16 18:16:32 +0600

    #checkout_page

--- src/app/components/onepagecheckout/onepagecheckout.component.ts
+++ src/app/components/onepagecheckout/onepagecheckout.component.ts
@@ -10,6 +10,50 @@ export class OnepagecheckoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.CartDetails();
+    this.loadCart();
+    this.loadcheckoutTotal() ;
   }
+  getCartDetailss:any=[];
+
+  CartDetails(){
+    if(localStorage.getItem('localCart'))
+    {
+this.getCartDetailss= JSON.parse(localStorage.getItem('localCart')!);
+//console.log(this.getCartDetails);
+
+
+    }
+
+  }
+
+
+   //sub-Total taka section
+   total:number =0
+   loadCart(){
+     if(localStorage.getItem('localCart')){
+       this.getCartDetailss= JSON.parse(localStorage.getItem('localCart')!);
+     this.total= this.getCartDetailss.reduce(function(acc:any,val:any){
+        return acc+ (val.amt * val.qnt ) ;
+
+     },0);
+
+     }
+
+   }
+
+   //checkoutcart-Total taka section
+   checkoutTotal:number =0;
+   loadcheckoutTotal(){
+     if(localStorage.getItem('localCart')){
+       this.getCartDetailss= JSON.parse(localStorage.getItem('localCart')!);
+     this.checkoutTotal= this.getCartDetailss.reduce(function(acc:any,val:any){
+        return acc+ ((val.amt * val.qnt) ) ;
+
+     },60);
+
+     }
+
+   }
 
 }
